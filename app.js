e14396a9914886ae64e84fad236388d66e39b91d
vale1b <barberisvalentin85@gmail.com>
2023-11-24 06:52:15 -0300

    Subiendo archivos

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+const port = process.env.PORT || 3030;
+app.listen(port, () => console.log("Servidor corriendo en el puerto ${port}"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/login", (req, res) => {
+    let htmlPath = path.resolve(__dirname, "./views/login.html");
+    res.sendFile(htmlPath)
+})
+
+app.post("/login", (req, res) => {
+    res.send("Llego el formulario de ingreso");
+})
+
+app.get("/register", (req, res) => {
+    let htmlPath = path.resolve(__dirname, "./views/register.html");
+    res.sendFile(htmlPath)
+})
+
+app.post("/register", (req, res) => {
+    res.send("Llegó el formulario de registro");
+})
+
+
