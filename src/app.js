6ccfe6c21c2c97cf23c5a16114cfe29d8001b3c6
vale1b <barberisvalentin85@gmail.com>
2023-11-28 18:20:59 -0300

    Corrigiendo archivos

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 const port = process.env.PORT || 3030;
-app.listen(port, () => console.log("Servidor corriendo en el puerto ${port}"));
+app.listen(port, () => console.log("Servidor corriendo en el puerto 3030"));
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
